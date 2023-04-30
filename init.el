febe6a90e317844efd1d35e3c13ac754c3b5810a
Matthew Rothlisberger <mattjrothlis@gmail.com>
2023-04-30 15:55:01 -0400

    adds a couple packages and improves settings

--- init.el
+++ init.el
@@ -4,6 +4,9 @@
 ;; disable the toolbar
 (tool-bar-mode -1)
 
+;; get font size back to normal under dwm
+(set-face-attribute 'default nil :height 93)
+
 ;; enable Common Lisp features
 (require 'cl)
 
@@ -12,7 +15,7 @@
 (add-to-list 'package-archives
              '("melpa" . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("org" . "https://orgmode.org/elpa/") t)
+             '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 
 (package-initialize)
 
@@ -25,10 +28,14 @@
 (eval-when-compile
   (require 'use-package))
 
-;; load org and the full config file
-(require 'org)
+(use-package sqlite3)
+
+;; load org and my full config file
 (use-package org
+  :pin    gnu
   :ensure org-plus-contrib)
+
+(require 'org)
 
 (org-babel-load-file "~/.emacs.d/config.org")
 
@@ -44,9 +51,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(flycheck-checker-error-threshold 500)
+ '(lsp-ui-doc-show-with-cursor t)
  '(org-agenda-files '("~/org/notes.org" "~/.notes"))
  '(package-selected-packages
-   '(glsl-mode browse-kill-ring projectile forge speed-type ox-hugo deadgrep lua-mode company flycheck all-the-icons-dired doom-modeline doom-themes all-the-icons auto-package-update use-package unfill org-plus-contrib org-mime magit ido-completing-read+ amx)))
+   '(sqlite3 markdown-mode magit-section lsp-ui lsp-mode eldoc org glsl-mode browse-kill-ring projectile forge speed-type ox-hugo deadgrep lua-mode company flycheck all-the-icons-dired doom-modeline doom-themes all-the-icons auto-package-update use-package unfill org-plus-contrib org-mime magit ido-completing-read+ amx)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
