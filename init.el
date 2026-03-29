e2f3ec720a2ebbef34bebf6fc4927d503142ce6e
Matthew Rothlisberger <mattjrothlis@gmail.com>
2026-03-29 16:17:39 -0400

    switches to Eglot LSP client

--- init.el
+++ init.el
@@ -52,21 +52,35 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(eglot-code-action-indications '(margin))
  '(flycheck-checker-error-threshold 500)
  '(ispell-complete-word-dict "/usr/share/dict/web2")
  '(lsp-ui-doc-show-with-cursor t)
  '(max-lisp-eval-depth 800)
  '(org-agenda-files '("/home/matthew/org/notes.org"))
- '(package-selected-packages nil))
+ '(package-selected-packages nil)
+ '(package-vc-selected-packages
+   '((eglot-booster :vc-backend Git :url
+                    "https://github.com/jdtsmith/eglot-booster")))
+ '(treesit-font-lock-level 4))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(aw-leading-char-face ((t (:inherit ace-jump-face-foreground :foreground "yellow" :weight semi-bold :slant normal :underline nil :height 4.0))))
- '(bold ((t (:foreground "#D0D0D0" :weight bold))))
+ '(bold ((t (:weight bold))))
+ '(eglot-semantic-declaration ((t (:foreground "navajo white"))))
+ '(eglot-semantic-function ((t (:foreground "khaki1"))))
+ '(eglot-semantic-method ((t (:foreground "LightPink1"))))
+ '(eglot-semantic-number ((t (:inherit font-lock-number-face))))
+ '(eglot-semantic-operator ((t (:foreground "plum1"))))
+ '(font-lock-comment-face ((t (:background "#191a21" :foreground "MistyRose4" :slant italic))))
+ '(font-lock-number-face ((t (:foreground "violet"))))
+ '(font-lock-operator-face ((t (:foreground "white smoke" :weight bold))))
+ '(gnus-group-news-low ((t (:weight bold))))
+ '(gnus-group-news-low-empty ((t (:weight normal))))
+ '(markdown-code-face ((t (:extend t :background "gray15"))))
  '(org-done ((t (:inherit org-headline-done :weight bold))))
  '(org-table ((t (:foreground "gray"))))
- '(org-todo ((t (:foreground "#79D836" :weight bold))))
- '(gnus-group-news-low ((t (:weight bold))))
- '(gnus-group-news-low-empty ((t (:weight normal)))))
+ '(org-todo ((t (:foreground "#79D836" :weight bold)))))
